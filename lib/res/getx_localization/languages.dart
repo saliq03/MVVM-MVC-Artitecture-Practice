f5b3cb6a99b178b255a617f7c398d7dc7e6f8d70
saliq03 <javidsaliq@gmail.com>
2024-09-02 20:18:29 +0530

    getX localization

---
+++ lib/res/getx_localization/languages.dart
@@ -0,0 +1,15 @@
+import 'package:get/get.dart';
+
+class Languages extends Translations{
+  @override
+  Map<String, Map<String, String>> get keys => {
+    'en_US':{
+      'email hint': 'Enter email'
+    },
+    'ur_PK':{
+      'email hint': 'ای میل درج کریں'
+    },
+
+  };
+
+}
---
+++ lib/view/splash_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('email hint'.tr),
+      ),
+    );
+  }
+}
