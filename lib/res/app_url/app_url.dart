ea414ea181edf9ea1095f994acb8d7e278d53b78
saliq03 <javidsaliq@gmail.com>
2024-09-11 23:59:12 +0530

    get api

--- lib/res/app_url/app_url.dart
+++ lib/res/app_url/app_url.dart
@@ -2,4 +2,5 @@ class AppUrl{
   static const String baseUrl='https://reqres.in';
 
   static const String loginApi='$baseUrl/api/login';
+  static const String userListApi='https://webhook.site/0d4aa637-d41a-4ce8-8124-67719198feec';
 }
